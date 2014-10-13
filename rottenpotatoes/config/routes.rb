c0ab7326f04f91089da6c447e4688a6fb678f1bc
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2014-10-13 07:52:55 -0300

    Scenario 02 finished

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -12,6 +12,7 @@ Rottenpotatoes::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  match 'movies/:id/same_director' => 'movies#same_director', as: :same_director_movie
   resources :movies
 
   # Sample resource route with options:
